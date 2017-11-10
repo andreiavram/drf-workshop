8bcf6a60974620037baf2ce02988e18e0e2cb357
Andrei Avram <andrei.avram@spyhce.com>
2017-11-10 16:14:21 +0200

    added drf

--- web/drfworkshop/settings.py
+++ web/drfworkshop/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'workshop.apps.WorkshopConfig',
+
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- web/workshop/models.py
+++ web/workshop/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class Device(models.Model):
     DEVICE_READ = 1
     DEVICE_WRITE = 0
