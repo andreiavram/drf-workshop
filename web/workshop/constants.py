f8ad6f7ce7f4d5d890b333c6e1f13e443ffba923
candale <candale.andrei@gmail.com>
2017-11-10 22:53:29 +0200

    Added models and fixtures

---
+++ web/workshop/constants.py
@@ -0,0 +1,36 @@
+from operator import lt, le, gt, ge, eq, ne
+
+
+class MessageDirection(object):
+
+    DEVICE_READ = 1
+    DEVICE_WRITE = 0
+
+    DEVICE_DIRECTIONS = (
+        (DEVICE_READ, 'data provider'),
+        (DEVICE_WRITE, 'data consumer')
+    )
+
+
+class Operation(object):
+
+    LT = '<'
+    LE = '<='
+    GT = '>'
+    GE = '>='
+    EQ = '='
+    NE = '!='
+
+    OPERATIONS = (
+        (LT, 'less than'),
+        (LE, 'less than or equal'),
+        (GT, 'greater than'),
+        (GE, 'greater than or equal'),
+        (EQ, 'equal'),
+        (NE, 'not equal')
+    )
+
+    def eval_for_op(op, a, b):
+        mapping = {'<': lt, '<=': le, '>': gt, '>=': ge, '=': eq, '!=': ne}
+
+        return mapping[op](a, b)
