9d17301f9a198e744fa9a8fb2e8dcfd01ff584b7
candale <candale.andrei@gmail.com>
2017-11-10 22:53:29 +0200

    Added models and fixtures

--- web/workshop/admin.py
+++ web/workshop/admin.py
@@ -1,11 +1,30 @@
 from django.contrib import admin
 
 # Register your models here.
-from workshop.models import Device
+from workshop.models import Device, MessageHistory, RuleSet, Rule
 
 
 class DeviceAdmin(admin.ModelAdmin):
-    list_display = ["name", "channel"]
+
+    list_display = ('name', 'topic', 'direction', 'description')
 
 
-admin.register(DeviceAdmin, Device)
+class MessageHistoryAdmin(admin.ModelAdmin):
+
+    list_display = ['device', 'value']
+
+
+class RuleSetAdmin(admin.ModelAdmin):
+
+    list_display = ['target_device', 'payload']
+
+
+class RuleAdmin(admin.ModelAdmin):
+
+    list_display = ['ruleset', 'source_device', 'ref_value', 'operator']
+
+
+admin.site.register(Device, DeviceAdmin)
+admin.site.register(MessageHistory, MessageHistoryAdmin)
+admin.site.register(RuleSet, RuleSetAdmin)
+admin.site.register(Rule, RuleAdmin)
--- web/workshop/models.py
+++ web/workshop/models.py
@@ -1,38 +1,64 @@
-from django.db import models
 import time
 import random
-# Create your models here.
+
+from django.db import models
+from device_client import DeviceController
+
+from workshop.constants import MessageDirection, Operation
 
 
 class Device(models.Model):
-    DEVICE_READ = 1
-    DEVICE_WRITE = 0
-
-    DEVICE_DIRECTION = ((DEVICE_READ, "data provider"), (DEVICE_WRITE, "data consumer"))
 
     name = models.CharField(max_length=255)
     topic = models.CharField(max_length=255)
-    direction = models.IntegerField(choices=DEVICE_DIRECTION)
+    direction = models.IntegerField(choices=MessageDirection.DEVICE_DIRECTIONS)
+    description = models.TextField(null=True, blank=True)
 
     def read(self):
-        pass
+        if self.direction == self.DEVICE_WRITE:
+            raise ValueError('This device is only for writing')
+
+        with DeviceController(self.name) as device:
+            return device.read(self.topic)
 
     def write(self, data):
-        pass
+        if not all([value.isdigit() for value in data.split(',')]):
+            raise ValueError('Data may be only a CSV list of numbers')
+
+        if self.direction == self.DEVICE_READ:
+            raise ValueError('This device is only for reading')
+
+        with DeviceController(self.name) as device:
+            device.write(self.topic, data)
 
     def is_alive(self) -> bool:
         #   this simulates an expensive get status operation :)
         time.sleep(1)
         return bool(random.getrandbits(1))
 
+    def __str__(self):
+        return self.name
+
 
 class MessageHistory(models.Model):
-    pass
+
+    device = models.ForeignKey(Device)
+    value = models.CharField(max_length=100)
 
 
 class RuleSet(models.Model):
-    pass
+
+    target_device = models.ForeignKey(Device)
+    payload = models.CharField(max_length=100)
+
+    def __str__(self):
+        return 'Device: {}. Payload: {}'.format(
+            self.target_device, self.payload)
 
 
 class Rule(models.Model):
-    pass
+
+    ruleset = models.ForeignKey(RuleSet, related_name='rules')
+    source_device = models.ForeignKey(Device)
+    ref_value = models.FloatField()
+    operator = models.SmallIntegerField(choices=Operation.OPERATIONS)
