5498c0814c9fe64c301e0f43e481af2d416383c4
Andrei Avram <andrei.avram@spyhce.com>
2017-11-10 16:04:31 +0200

    added models

--- web/workshop/admin.py
+++ web/workshop/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from workshop.models import Device
+
+
+class DeviceAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Device
+
+
+admin.register(DeviceAdmin, Device)
--- web/workshop/models.py
+++ web/workshop/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+class Device(models.Model):
+    DEVICE_READ = 1
+    DEVICE_WRITE = 0
+
+    DEVICE_DIRECTION = ((DEVICE_READ, "data provider"), (DEVICE_WRITE, "data consumer"))
+
+    name = models.CharField(max_length=255)
+    topic = models.CharField(max_length=255)
+    direction = models.IntegerField(choices=DEVICE_DIRECTION)
+
+    def read(self):
+        pass
+
+    def write(self, data):
+        pass
+
+
+class MessageHistory(models.Model):
+    pass
+
+
+class RuleSet(models.Model):
+    pass
+
+
+class Rule(models.Model):
+    pass
